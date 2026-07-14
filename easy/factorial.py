eb151884833474f5a6a004efa59c8746311bcc7d
Raghu <radhdrlng8@gmail.com>
2026-07-14 13:21:10 +0530

    new for loop added

--- easy/factorial.py
+++ easy/factorial.py
@@ -1,5 +1,14 @@
+n = 5
 def factorial(n):
     if n <= 1:
         return 1
     return n*factorial(n-1)
-print(factorial(5))
+print(factorial(n))
+
+
+fac_output = 1
+
+for i in range(n):
+    fac_output = fac_output *(i+1)
+print(fac_output)
+
