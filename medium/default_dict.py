3a6d736b20a9539876e6f45aeaef1368bd23c735
Raghu <radhdrlng8@gmail.com>
2026-08-20 00:46:59 -0700

    medium/dict

---
+++ medium/default_dict.py
@@ -0,0 +1,26 @@
+#defaultdict : "if a key doesn't exist, automatically create it with this default value." "defaultdict is best at:Grouping — collecting multiple items under a shared key."
+
+
+from collections import defaultdict
+students = [
+    ("Alice", "A"),
+    ("Bob", "B"),
+    ("Charlie", "A"),
+    ("David", "C"),
+    ("Eve", "B"),
+    ("Frank", "A"),
+]
+
+group = defaultdict(list)
+count = defaultdict(int)
+total = 1
+
+for x,y in students:
+    group[y].append(x)
+    count[y] += total
+print(group)
+print(type(group))
+
+print(count)
+print(type(count))
+print(dict(count))
